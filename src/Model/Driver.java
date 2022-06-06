60d3b6e7469153715f0a6abbd26be4ce28a9df26
SebastianBarra <barra20sebastian02p19@gmail.com>
2022-06-06 18:05:30 -0400

    Cree la clase Persona que es padre de driver y cliente, tambien cree la clase Driver, su view y controller

---
+++ src/Model/Driver.java
@@ -0,0 +1,96 @@
+package Model;
+
+/**
+ * This class represents the driver of the taxis.
+ *
+ * @author Sebastian Barra
+ */
+public class Driver extends Persona{
+
+
+    private int AgeDriver;
+    private int licenseNumber;
+    private int racesAmount;
+
+
+    /**
+     * This is the constructor method that receives information about the driver.
+     *
+     * @param name  how the driver is called
+     * @param phoneNumber the driver's phone number
+     * @param ageDriver how old is the driver
+     * @param licenseNumber license identification
+     * @param racesAmount total races made by the driver
+     */
+
+    public Driver(String name, int phoneNumber, int ageDriver, int licenseNumber, int racesAmount) {
+        super(name, phoneNumber);
+        AgeDriver = ageDriver;
+        this.licenseNumber = licenseNumber;
+        this.racesAmount = racesAmount;
+    }
+
+
+    /**
+     * This method gets the driver's age.
+     *
+     * @return AgeDriver
+     */
+
+    public int getAgeDriver() {
+        return AgeDriver;
+    }
+
+    /**
+     * This method returns the the driver's license number.
+     *
+     * @return licenseNumber
+     */
+
+    public int getLicenseNumber() {
+        return licenseNumber;
+    }
+
+
+    /**
+     * This method returns the total number of runs the driver made.
+     * @return racesAmount
+     */
+
+    public int getRacesAmount() {
+        return racesAmount;
+    }
+
+
+    /**
+     * This method sets an age to the driver.
+     *
+     * @param ageDriver how old is the driver
+     */
+
+    public void setAgeDriver(int ageDriver) {
+        AgeDriver = ageDriver;
+    }
+
+
+    /**
+     * This method saves the license number.
+     *
+     * @param licenseNumber license identification
+     */
+
+    public void setLicenseNumber(int licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+
+    /**
+     * This methos set the amount of races that the driver has.
+     *
+     * @param racesAmount total races made by the driver
+     */
+
+    public void setRacesAmount(int racesAmount) {
+        this.racesAmount = racesAmount;
+    }
+}
