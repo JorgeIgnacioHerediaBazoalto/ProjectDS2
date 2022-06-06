4d09a976ed2ce44efe2f0885051067e7dd85c310
Santiago Caballero <santicm95386b@gmail.com>
2022-06-06 18:09:51 -0400

    Utils package style classes and Printer methods

---
+++ src/Utils/PrintStyle.java
@@ -0,0 +1,35 @@
+package Utils;
+
+/**
+ * This is a util class to store text style values
+ * This class contains constants values of different types of text style to be used in console prints
+ * The style options are to create a box around the text, to strike the text and underline the text.
+ *
+ * @author Santiago Caballero
+ */
+
+public class PrintStyle {
+
+    //Text reset
+    public  final String RESET = "\033[0m";
+    //box
+    public  final String BOXING = "\033[0;51m";   // BLACK
+
+    //strikeout
+    public  final String BLACK_STRIKE = "\033[9;30m";   // BLACK
+    public  final String RED_STRIKE = "\033[9;31m";     // RED
+    public  final String GREEN_STRIKE = "\033[9;32m";   // GREEN
+    public  final String YELLOW_STRIKE = "\033[9;33m";  // YELLOW
+    public  final String BLUE_STRIKE = "\033[9;34m";    // BLUE
+    public  final String WHITE_STRIKE = "\033[9;37m";   // WHITE
+
+    //underline
+    public  final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
+    public  final String RED_UNDERLINED = "\033[4;31m";    // RED
+    public  final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
+    public  final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
+    public  final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
+    public  final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
+    public  final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
+    public  final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
+}
--- src/Utils/Printer.java
+++ src/Utils/Printer.java
@@ -1,16 +1,68 @@
 package Utils;
+import java.util.Map;
 
+/**
+ * This is class Printer is used to print a message or messages on the console output
+ * This class belongs to utils package and it interacts with view classes
+ *
+ * @author Santiago Caballero
+ */
 public class Printer {
+    private final Colors colors;
+    private final PrintStyle style;
 
+    /**
+     * This is the constructor method of the class Printer
+     * It instances the class Colors and PrintStyle to modify the print style
+     *
+     */
+    public Printer() {
+        colors = new Colors();
+        style = new PrintStyle();
+    }
+
+    /**
+     * Method to print a message by console
+     *
+     * @param message a String value
+     */
     public void printMessage(String message) {
-        System.out.println(message);
+        System.out.println(colors.WHITE_BOLD_BRIGHT+message+colors.RESET);
     }
 
+
+    /**
+     * Method to print 2 messages by console
+     *
+     * @param message1 string value
+     * @param message2 string value
+     */
     public void printMessages(String message1, String message2) {
-        System.out.println(message1 + " " + message2);
+        System.out.println(colors.GREEN_BOLD_BRIGHT+message1+colors.RESET +
+                " " + style.BOXING+colors.WHITE_BOLD_BRIGHT+message2+colors.RESET+style.RESET);
     }
 
-    public void printMessages(String message, int valor) {
-        System.out.println(message+": "+valor);
+    /**
+     * Overloading the method to print 2 messages by console
+     *
+     * @param message string value
+     * @param value int value
+     */
+    public void printMessages(String message, int value) {
+        System.out.println(message+": "+value);
     }
+
+    /**
+     * Method to print a dictionary with its key and value showing like a list with style and colors
+     * it loops each key and value of the dictionary and prints them by console output
+     * key and value need to be string type
+     *
+     * @param dictionary map type
+     */
+    public void printDictionary(Map<String, String> dictionary) {
+        dictionary.forEach((key,value) ->
+                System.out.println(colors.GREEN_BOLD_BRIGHT+key+colors.RESET+": "
+                        +colors.WHITE_BOLD_BRIGHT+value+colors.RESET));
+    }
+
 }
